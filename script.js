6143847b4e11dbd117b711ecedc0319496995aa3
Sundrop123 <47043481+Sundrop123@users.noreply.github.com>
2019-03-22 09:40:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 'use strict'
 
-/*App will take input from user for the number of states and the maximum number of results
-
-Based on the user input, the app will call National Parks API and render name, description, address, and website url results on page
-*/
-
-//from user input, create api url with query parameters
 function formatQueryUrl(queryParam){
   const npsApi = 'https://developer.nps.gov/api/v1/parks?';
   const queryString = Object.keys(queryParam).map(key=>encodeURIComponent(key)+'='+encodeURIComponent(queryParam[key])).join('&');
@@ -14,7 +8,6 @@ function formatQueryUrl(queryParam){
 
 };
 
-//get results based on user input using fetch function. check if call is successful, if true, display results. if not, display error mesasge
 function getResults(stateCode,maxResults=10){
   const param = {
     api_key:'4sC0qMZ7q0QFCeSm8v2zJIlMHughxVfGNZU3Np77',
@@ -53,7 +46,6 @@ function displayResults(responseJson){
   $('section').removeClass('hidden')
 };
 
-//watch for submit button then get values
 function watchSubmit(){
   $('form').on('submit',event=>{
     event.preventDefault();
